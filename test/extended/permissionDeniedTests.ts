98bc9eadec6454b29407b4625f98fbcae6801437
gliviu <grigoresculiviu@gmail.com>
2021-05-01 20:36:30 +0300

    tests

--- test/extended/permissionDeniedTests.ts
+++ test/extended/permissionDeniedTests.ts
@@ -3,6 +3,7 @@ import print from '../print'
 import Streams from 'memory-streams'
 import { readFileSync } from "fs"
 import { join } from "path"
+import { StreamState } from "http2"
 
 interface Test {
     testId: string,
@@ -15,7 +16,7 @@ interface Test {
 const tests: Test[] = [
     {
         testId: '001',
-        description: 'Should handle permission denied errors when handlePermissionDenied option is used',
+        description: 'Should handle permission denied errors when options.handlePermissionDenied is enabled',
         left: '/tmp/37-perms-test/t1-files-and-dirs/a',
         right: '/tmp/37-perms-test/t1-files-and-dirs/b',
         options: { compareSize: true, compareContent: true, handlePermissionDenied: true },
@@ -23,30 +24,37 @@ const tests: Test[] = [
     {
         testId: '002',
         description: 'Should support links when dealing with permission denied errors',
-        left:  '/tmp/37-perms-test/t2-links/a',
+        left: '/tmp/37-perms-test/t2-links/a',
         right: '/tmp/37-perms-test/t2-links/b',
         options: { compareSize: true, compareContent: true, handlePermissionDenied: true },
     },
     {
         testId: '003',
         description: 'Should support forbidden root directories',
-        left:  '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/a',
+        left: '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/a',
         right: '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/b',
         options: { compareSize: true, compareContent: true, handlePermissionDenied: true },
     },
     {
         testId: '004',
         description: 'Should support forbidden root directories (reversed)',
-        left:  '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/b',
+        left: '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/b',
         right: '/tmp/37-perms-test/t3-root/t1-root-left-dir-ok,root-right-dir-forbidden/a',
         options: { compareSize: true, compareContent: true, handlePermissionDenied: true },
     },
     {
         testId: '005',
         description: 'Should support forbidden root files',
-        left:  '/tmp/37-perms-test/t3-root/t2-root-left-file-ok,root-right-file-forbidden.txt/a/test.txt',
+        left: '/tmp/37-perms-test/t3-root/t2-root-left-file-ok,root-right-file-forbidden.txt/a/test.txt',
         right: '/tmp/37-perms-test/t3-root/t2-root-left-file-ok,root-right-file-forbidden.txt/b/test.txt',
         options: { compareSize: true, compareContent: true, handlePermissionDenied: true },
+    },
+    {
+        testId: '010',
+        description: 'Should not handle permission denied errors when options.handlePermissionDenied is disabled',
+        left: '/tmp/37-perms-test/t1-files-and-dirs/a',
+        right: '/tmp/37-perms-test/t1-files-and-dirs/b',
+        options: { compareSize: true, compareContent: true, handlePermissionDenied: false },
     },
 
 ]
@@ -54,19 +62,20 @@ const tests: Test[] = [
 type CompareFn = (left: string, right: string, options: Options) => Promise<Result> | Result
 
 async function runSingleTest(test: Test, compareFn: CompareFn) {
-    const t1 = Date.now()
-    const compareResult = await compareFn(test.left, test.right, test.options)
-    const t2 = Date.now()
-    const writer = new Streams.WritableStream()
-    print(compareResult, writer, { showAll: true, wholeReport: true, reason: true})
-    const compareResultStr = writer.toString()
-    const duration = (t2 - t1) / 1000
+    let outputWriter = new Streams.WritableStream()
+    try {
+        const compareResult = await compareFn(test.left, test.right, test.options)
+        print(compareResult, outputWriter, { showAll: true, wholeReport: true, reason: true })
+    } catch (error) {
+        outputWriter.write(error.toString())
+    }
+    const compareResultStr = outputWriter.toString()
     const expectedFilePath = join(__dirname, 'res', '37-perms-expected', `${test.testId}.txt`)
     const expected = readFileSync(expectedFilePath).toString()
     const ok = compareResultStr === expected
-    const testResult = ok ? `ok ${duration} s` : 'fail'
+    const testResult = ok ? `OK` : 'FAIL'
     console.log(`${test.testId} ${test.description}: ${testResult}`)
-    if (test.testId === '001') {
+    if (test.testId === '010') {
         // console.log(compareResultStr)
         // console.log(expected)
     }
