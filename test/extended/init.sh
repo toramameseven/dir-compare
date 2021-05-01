51cf994b1fbfecda01069c15931ba27c858cfbc1
gliviu <grigoresculiviu@gmail.com>
2021-05-02 00:29:35 +0300

    tests

--- test/extended/init.sh
+++ test/extended/init.sh
@@ -11,35 +11,35 @@ if [[ ! 'root' == $(whoami) ]]; then echo 'Run as root.'; exit 1; fi;
 #     node $ROOTDIR/build/test/extract.js
 # fi
 
-# # Download and extract linux kernels
-# l1=linux-4.3
-# l2=linux-4.4
-# mkdir -p /tmp/dircompare
-# if [ ! -f /tmp/dircompare/$l1.tar.gz ]; then
-#   echo Downloading /tmp/dircompare/$l1.tar.gz
-#   curl https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/$l1.tar.gz --output /tmp/dircompare/$l1.tar.gz
-# fi
-# if [ ! -f /tmp/dircompare/$l2.tar.gz ]; then
-#   echo Downloading /tmp/dircompare/$l2.tar.gz
-#   curl https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/$l2.tar.gz --output /tmp/dircompare/$l2.tar.gz
-# fi
+# Download and extract linux kernels
+l1=linux-4.3
+l2=linux-4.4
+mkdir -p /tmp/dircompare
+if [ ! -f /tmp/dircompare/$l1.tar.gz ]; then
+  echo Downloading /tmp/dircompare/$l1.tar.gz
+  curl https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/$l1.tar.gz --output /tmp/dircompare/$l1.tar.gz
+fi
+if [ ! -f /tmp/dircompare/$l2.tar.gz ]; then
+  echo Downloading /tmp/dircompare/$l2.tar.gz
+  curl https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/$l2.tar.gz --output /tmp/dircompare/$l2.tar.gz
+fi
 
-# if [ ! -d /tmp/$l1 ]; then
-#   echo Extracting into /tmp/$l1
-#   tar -xzf /tmp/dircompare/$l1.tar.gz -C /tmp
-#   if [[ $? -ne 0 ]]; then
-#       echo Extracting $l1 failed
-#       exit 1
-#   fi
-# fi
-# if [ ! -d /tmp/$l2 ]; then
-#   echo Extracting into /tmp/$l2
-#   tar -xzf /tmp/dircompare/$l2.tar.gz -C /tmp
-#   if [[ $? -ne 0 ]]; then
-#       echo Extracting $l2 failed
-#       exit 1
-#   fi
-# fi
+if [ ! -d /tmp/$l1 ]; then
+  echo Extracting into /tmp/$l1
+  tar -xzf /tmp/dircompare/$l1.tar.gz -C /tmp
+  if [[ $? -ne 0 ]]; then
+      echo Extracting $l1 failed
+      exit 1
+  fi
+fi
+if [ ! -d /tmp/$l2 ]; then
+  echo Extracting into /tmp/$l2
+  tar -xzf /tmp/dircompare/$l2.tar.gz -C /tmp
+  if [[ $? -ne 0 ]]; then
+      echo Extracting $l2 failed
+      exit 1
+  fi
+fi
 
 # Extract dir for permission denied tests
 PERMISSION_DENIED_TEST_FILE=37-perms-test
