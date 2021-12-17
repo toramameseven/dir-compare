98c25240b44728b29779e646679a22f56a964d82
gliviu <grigoresculiviu@gmail.com>
2021-12-17 10:36:56 +0200

    Switch to typescript

--- test/extended/compare.ts
+++ test/extended/compare.ts
@@ -1,4 +1,5 @@
 import { compareSync, compare, Options, fileCompareHandlers, Result } from "../../src"
+import { deepCompare } from "./deepCompare"
 
 interface Test {
     testId: string,
@@ -83,29 +84,6 @@ const tests: Test[] = [
 ]
 
 type CompareFn = (left: string, right: string, options: Options) => Promise<Result> | Result
-
-const deepCompare = function(obj1, obj2) {
-    const obj1Keys = Object.keys(obj1);
-    const obj2Keys = Object.keys(obj2);
-    const allKeys = new Set([...obj1Keys, ...obj2Keys])
-
-    for (const objKey of allKeys) {
-        if (obj1[objKey] !== obj2[objKey]) {
-            if(typeof obj1[objKey] == "object" && typeof obj2[objKey] == "object") {
-                if(!deepCompare(obj1[objKey], obj2[objKey])) {
-                    console.log(objKey)
-                    return false;
-                }
-            } 
-            else {
-                console.log('y')
-                return false;
-            }
-        }
-    }
-
-    return true;
-};
 
 async function runSingleTest(test: Test, compareFn: CompareFn) {
     const t1 = Date.now()
